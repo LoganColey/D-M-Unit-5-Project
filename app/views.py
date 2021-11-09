34dd0d56ebf3c8321d2d82b0ec6a2ea4b13e8a9a
LoganColey <lcoley22@basecampcodingacademy.org>
2021-11-09 13:05:06 -0600

    Update views.py

--- app/views.py
+++ app/views.py
@@ -23,7 +23,7 @@ def registerPagePly(request):
 				username = form.cleaned_data.get('username')
 
 				group = Group.objects.get(name='Player')
-				User.groups.add(group)
+				user.groups.add(group)
 
 				messages.success(request, f" Player: {username} + has created Account")
 				return redirect('login')
@@ -76,22 +76,75 @@ def Pview(request):
 
 @login_required(login_url='login')
 def createmonsterview(request):
-        form = CreateMonsterForm()
-        context = {'form': form}
-        if request.method == "POST":
-            form = CreateMonsterForm(request.POST)
-            if form.is_valid():
-                form.save()
-                return redirect('userpage')
-        return render(request,'create_monster.html',context)
+	form = CreateMonsterForm()
+	context = {'form': form}
+	if request.method == "POST":
+		form = CreateMonsterForm(request.POST)
+		if form.is_valid():
+			monster = form.save()
+			monster.creator = request.user
+			monster.save()
+			return redirect('DMview')
+	return render(request,'create_monster.html',context)
 
 @login_required(login_url='login')
 def createcharacterview(request):
-    form = CreateCharacterForm()
-    context = {'form': form}
-    if request.method == "POST":
-        form = CreateCharacterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('userpage')
-    return render(request, 'create_character.html', context)
+		form = CreateCharacterForm()
+		context = {'form': form}
+		if request.method == "POST":
+			form = CreateCharacterForm(request.POST)
+			if form.is_valid():
+					character = form.save()
+					character.creator = request.user
+					character.save()
+		return render(request, 'create_character.html', context)
+
+@login_required(login_url='login')
+def updateCharacter(request, pk):
+
+	order = Character.objects.get(id=pk)
+	form = CreateCharacterForm(instance=order)
+
+	if request.method == 'POST':
+		form = CreateCharacterForm(request.POST, instance=order)
+		if form.is_valid():
+			form.save()
+			return redirect('home')
+
+	context = {'form':form}
+	return render(request, 'update_ch.html', context)
+
+@login_required(login_url='login')
+def deleteCharacter(request, pk):
+	character = Character.objects.get(id=pk)
+	if request.method == "POST":
+		character.delete()
+		return redirect('home')
+
+	context = {'item':character}
+	return render(request, 'delete_ch.html', context)
+
+@login_required(login_url='login')
+def updateMonster(request, pk):
+
+	order = Monster.objects.get(id=pk)
+	form = CreateMonsterForm(instance=order)
+
+	if request.method == 'POST':
+		form = CreateMonsterForm(request.POST, instance=order)
+		if form.is_valid():
+			form.save()
+			return redirect('home')
+
+	context = {'form':form}
+	return render(request, 'update_m.html', context)
+
+@login_required(login_url='login')
+def deleteMonster(request, pk):
+	monster = Monster.objects.get(id=pk)
+	if request.method == "POST":
+		monster.delete()
+		return redirect('home')
+
+	context = {'item':monster}
+	return render(request, 'delete_m.html', context)
